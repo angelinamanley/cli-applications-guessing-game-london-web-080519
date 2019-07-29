c0bdd10fe208e1f49923ae7c0d1d4035ce6c4344
Angelina Shin Manley <ashin05@gmail.com>
2019-07-29 11:50:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -9,7 +9,7 @@ def run_guessing_game
       puts "You guessed the correct number!"
     elsif input == "exit"
       puts "Goodbye!"
-    else
+    elsif input != random
       puts "Sorry! The computer guessed #{random}"
   end
 
